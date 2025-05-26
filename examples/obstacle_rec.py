06eee13a397ee7978da4f8dab0fca3780127185a
ClemouDouard <clementleveque911@gmail.com>
2025-05-26 16:35:48 +0200

    new photos

--- examples/obstacle_rec.py
+++ examples/obstacle_rec.py
@@ -1,8 +1,11 @@
 import torch
 from torchvision import transforms
 from PIL import Image
+from picarx import Picarx
+from pygame import time
 
 from self_driving_car.models.forward import ForwardClassifier
+from camera import Camera
 
 model = ForwardClassifier()
 model.load_state_dict(torch.load("../forward_classifier.pth"))
@@ -15,9 +18,19 @@ transform = transforms.Compose([
     transforms.ToTensor(),
 ])
 
+px = Picarx()
+clock = time.Clock()
+camera = Camera(
+    size=(640, 480),  # Resolution (width, height)
+    vflip=False,  # Vertical flip
+    hflip=False  # Horizontal flip
+)
+camera.start()
+
 while running:
-    ### IMG GETTING ###
-    ### img = 
+    
+    camera.take_photo("live_photo")
+    img = Image.open("live_photo.jpg").convert("RGB")
     img_tensor = transform(img).unsqueeze(0)
 
     with torch.no_grad():
@@ -26,7 +39,9 @@ while running:
     
     if predicted.item() == 0:
         print("car can go")
-        ### Forward car ###
+        px.forward(1)
     else:
         print("car cannot go")
         ### IMPL other img classifier
+    
+    clock.tick(20)
