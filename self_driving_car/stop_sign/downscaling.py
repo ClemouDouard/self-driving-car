c71c023114eb644a5abeefbe1b3771c5072f5871
ClemouDouard <clementleveque911@gmail.com>
2025-06-23 16:07:28 +0200

    Added more downscaled images

--- self_driving_car/stop_sign/downscaling.py
+++ self_driving_car/stop_sign/downscaling.py
@@ -1,14 +1,14 @@
 from PIL import Image
 import os
 
-dir = "self_driving_car/stop_sign/drive-download-20250623T132809Z-1-001"
+dir = "self_driving_car/stop_sign/parking_sign_raw"
 num_im = 1
 
 for file in os.listdir(dir):
     filename = os.fsdecode(file)
-    im = Image.open(rf"self_driving_car/stop_sign/drive-download-20250623T132809Z-1-001/{filename}")
+    im = Image.open(rf"{dir}/{filename}")
 
     im = im.resize((640,480))
-    im.save(f"self_driving_car/stop_sign/stop_sign{num_im}.jpg")
+    im.save(f"self_driving_car/stop_sign/parking_sign{num_im}.jpg")
 
     num_im += 1
